cb80e2f19c3fd01de0d0295a4f48a1c08b2e3b86
pdavidsonFIA <peterjd41@gmail.com>
2021-08-11 20:53:27 +0200

    refining manmade reinsurance

--- solvency2sf/scr_nl/cat/manmade/manmade.py
+++ solvency2sf/scr_nl/cat/manmade/manmade.py
@@ -55,53 +55,54 @@ def liab_div_loss(losses: np.array):
     return div_loss
 
 
-def liab(liab_vol):
-
+def liab(liab_vol, programs, covers):
+    """ Main mam-made liability SCR function """
     gross_losses = liab_gross_losses(liab_vol)
 
     # Need full index for each group to apply correlation:
-    gl = gross_losses['gross_loss'].groupby(by='grp_liab').sum().reindex(range(1,6)).rename('loss').fillna(0).array
+    gl = gross_losses['gross_loss'].groupby(by=['grp_liab']).sum().reindex(range(1,6)).rename('loss').fillna(0).array
+
     div_gross = liab_div_loss(gl)
 
-    # TODO: implement mitigation:
-    net_losses = gross_losses['gross_loss'].rename('net_loss')
+    net_losses = manmade_reinsurance(gross_losses, programs, covers)
     nl = net_losses.groupby(by='grp_liab').sum().reindex(range(1, 6)).rename('loss').fillna(0).array
-    div_net = liab_div_loss(gl)
-    return div_net
+    div_net = liab_div_loss(nl)
+    return pd.DataFrame.from_dict({'gross_loss': div_gross, 'net_loss': div_net}, orient='index', columns=['scr_liab']).T
 
 
-def example_reinsurance(
-        scen_loss,
+def manmade_reinsurance(
+        gross_losses,
         programs=pd.DataFrame.from_dict({'p1': {'xol_xs': 10, 'xol_limit': 20, 'reinstatement': 0.5, 'qs': 0.6},
                                          'p2': {'xol_xs': 5, 'xol_limit': 10, 'reinstatement': 0.25, 'qs': 0.8}},
                                         'index'),
-        program_covers=pd.DataFrame.from_dict({'DE': 'p1', 'CH': 'p1', 'PL': 'p2'}, 'index', columns=['prog_id'])
+        covers=pd.DataFrame.from_dict({'DE': 'p1', 'CH': 'p1', 'PL': 'p2'}, 'index', columns=['prog_id'])
 ):
     """
     Simplified example to produce net losses for each county allowing for multi-country reinsurance programs
     """
 
-    gross = scen_loss.to_frame()
-    gross = gross.merge(program_covers,how='left', left_on='country_isocode', right_index=True)
-    prog_loss = gross.groupby(['hazard', 'scenario', 'loss_event', 'prog_id']).sum()
+    gross = gross_losses.reset_index()
+    gross = gross.merge(covers, how='left', left_on='country', right_index=True)
+    prog_loss = gross.groupby(['prog_id']).sum()
     prog_loss = prog_loss.merge(programs, how='left', left_on='prog_id', right_index=True)
 
     prog_loss['xol_rec'] = prog_loss.apply(lambda x:
                                            -min(x.xol_limit - x.xol_xs, max(0, x.gross_loss - x.xol_xs)),
                                            axis=1)
+    # TODO: verify logic for reinstatement premiums on liability claims
     prog_loss['reins'] = - prog_loss.xol_rec / (prog_loss.xol_limit - prog_loss.xol_xs) * prog_loss.reinstatement
     prog_loss['net_xol'] = prog_loss[['gross_loss', 'xol_rec', 'reins']].sum(axis=1)
     prog_loss['qs_rec'] = -prog_loss.net_xol * prog_loss.qs
     prog_loss['net_loss'] = prog_loss['net_xol'] + prog_loss['qs_rec']
     prog_loss = prog_loss[['net_loss']]
 
-    gross['prog_tot'] = gross.groupby(by=['hazard', 'scenario', 'loss_event', 'prog_id']).transform(sum)
+    gross['prog_tot'] = gross[['gross_loss', 'prog_id']].groupby(by=['prog_id']).transform(sum)
     gross['pc_of_prog'] = gross.gross_loss/gross.prog_tot
-    gross = gross.reset_index().merge(prog_loss, how='left', on=['hazard', 'scenario', 'loss_event', 'prog_id'])
+    gross = gross.reset_index().merge(prog_loss, how='left', on=['prog_id'])
     gross.net_loss = gross.net_loss * gross.pc_of_prog
     # Filling for countries without a reinsurance program:
     gross.net_loss = gross.net_loss.fillna(gross.gross_loss)
-    gross = gross.set_index(['hazard', 'scenario', 'loss_event', 'country_isocode'])[['gross_loss', 'net_loss']].sort_index()
+    gross = gross.set_index(['country', 'grp_liab'])[['gross_loss', 'net_loss']].sort_index()
     return gross['net_loss']
 
 
