44d7339e31ff22f871a56a85d4b9b61ddd04a3d3
pdavidsonFIA <peterjd41@gmail.com>
2022-08-22 10:37:09 +0200

    Incrementing package version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as f:
 
 setuptools.setup(
     name="solvency2sf",
-    version="0.0.29",
+    version="0.0.30",
     author="Peter Davidson",
     author_email="peterjd41@gmail.com",
     description="Solvency 2 Standard Formula",
