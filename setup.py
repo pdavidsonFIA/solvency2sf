b5b2c161216d07423cfe73769149604d0599d8d6
peter.davidson <57438470+pdavidsonFIA@users.noreply.github.com>
2023-05-25 16:12:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as f:
 
 setuptools.setup(
     name="solvency2sf",
-    version="0.0.33",
+    version="0.0.34",
     author="Peter Davidson",
     author_email="peterjd41@gmail.com",
     description="Solvency 2 Standard Formula",
